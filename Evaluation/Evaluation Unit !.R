f1667585229650e8b90f42d8cc58d73d14244e7f
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2021-04-12 15:58:38 -0700

    Create Evaluation 1
    Se sube el archivo de la evaluacion para posteriormente subir el codigo ya funcional

---
+++ Evaluation/Evaluation Unit !.R
@@ -0,0 +1,15 @@
+################################################## 
+#                  Evaluation 1                  #
+##################################################
+
+# Developers 
+# Alvarez Yanez Jose Alonso 
+# Briseno Raul
+
+
+#You should generate a scatter-plot that shows the
+#Life expectancy statistics (y-axis) and fertility rate
+#(Fertility Rate -x axis) by country (Country).
+
+# Generate the vectors
+
