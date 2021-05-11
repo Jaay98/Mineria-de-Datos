356325605ee82b976e1d10853e57b332b241c67b
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2021-05-10 17:17:38 -0700

    Update Evaluation U2
    -se ajusta la carpeta de practicas
    -se sube media practica evaluatoria, falta solo la grafica la info ya esta filtrada y lista para usar
    -Falta documentacion deadme

---
+++ Evaluation/Evaluacion2.R
@@ -0,0 +1,25 @@
+################################################## 
+#                  Evaluation 2                  #
+##################################################
+
+#Export the library ggplot2
+
+library(ggplot2)
+
+#Import the dataframe
+df <- read.csv(file.choose())
+head(df)
+
+#Rename columns name
+colnames(df) <- c("Day_Week","Director","Genre","Title","Release_Date","Studio","Adjusted_Gross","Budget","Gross","IMDb_Rating","MovieLens_Rating","Overseas","Overseas%","Profit","Profit%","Runtime","US_dlls","GrossPor")
+
+#Delete useles data
+
+filter <- (df$Genre=="action" | df$Genre=="adventure" | df$Genre=="animation" | df$Genre=="comedy" | df$Genre=="drama") & (df$Studio=="Buena Vista Studios" | df$Studio=="Fox" | df$Studio=="Paramount Pictures" | df$Studio=="Sony" | df$Studio=="Universal" | df$Studio=="WB")
+
+filter_df <-df[filter,]
+
+#Show the new df
+
+head(filter_df)
+
